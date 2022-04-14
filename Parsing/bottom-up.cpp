559b2e870c7ec90ccae4388dc6fe6ae0c9825ac6
beedle2017 <choudhuriarka@gmail.com>
2022-04-14 21:10:07 +0530

    Transition Diagram set up

--- Parsing/bottom-up.cpp
+++ Parsing/bottom-up.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <set>
 
 using namespace std;
 
@@ -40,6 +41,11 @@ ostream & operator << (ostream &out, const sym &s)
 bool operator == (const sym &s1, const sym &s2)
 {
     return s1.s == s2.s;
+}
+
+bool operator < (const sym &s1, const sym &s2)
+{
+    return s1.s < s2.s;
 }
 
 struct production
@@ -122,15 +128,20 @@ bool does_exist(vector <production> &p, production s)
 
 struct item_set
 {
+    static int count;
+    int id;
     vector <production> prod_list;
 
     item_set()
     {
-        ;
+        id = count;
+        count ++;
     }
 
     item_set(vector <production> productions)
     {
+        id = count;
+        count ++;
         prod_list = productions;
     }
 
@@ -166,10 +177,95 @@ struct item_set
     }
 };
 
+int item_set::count = 0;
+
 ostream & operator << (ostream &out, const item_set &is)
 {
     for(auto prod : is.prod_list) cout << prod <<endl;
     return out;
+}
+
+bool operator == (item_set &is1, item_set &is2)
+{
+    if((int)(is1.prod_list.size()) != (int)(is2.prod_list.size())) return false;
+
+    int n = (int)(is1.prod_list.size());
+
+    for(int i = 0; i < n; i ++)
+    {
+        if(!(is1.prod_list[i] == is2.prod_list[i])) return false;
+    }
+
+    return true;
+}
+
+vector <pair<int,sym>> adj[1000];      // contains indices in the item_list
+
+vector <item_set> item_list;
+
+int item_in(item_set is)
+{
+    for(int i = 0; i < (int)(item_list.size()) ; i ++) if(item_list[i] == is) return i;
+    return -1;
+}
+
+vector <sym> nextsyms(item_set is)
+{
+    set <sym> clist;
+    for(auto prod : is.prod_list)
+    {
+        if(prod.dot_loc < prod.count_rhs())
+        {
+            clist.insert(prod.rhs[prod.dot_loc]);
+        }
+    }
+    vector <sym> rlist;
+    for(sym s : clist) rlist.push_back(s);
+
+    return rlist;
+}
+
+item_set transition(item_set is, sym s)
+{
+    item_set inext;
+    for(auto prod : is.prod_list)
+    {
+        if(prod.dot_loc < prod.count_rhs())
+        {
+            if(prod.rhs[prod.dot_loc] == s)
+            {
+                prod.dot_loc += 1;
+                inext.prod_list.push_back(prod);
+            }
+        }
+    }
+    inext.get_closure();
+    return inext;
+}
+
+void generate(item_set is)
+{
+    vector <sym> symlist = nextsyms(is);
+    int curridx = item_in(is);
+
+    for(sym s : symlist)
+    {
+        item_set nis = transition(is, s);
+
+        int idx;
+        if(item_in(nis) == -1)
+        {
+            item_list.push_back(nis);
+            idx = item_in(nis);
+            adj[curridx].push_back({idx, s});
+            generate(nis);
+        }
+        else
+        {
+            idx = item_in(nis);
+            adj[curridx].push_back({idx, s});
+        }
+    }
 }
 
 signed main()
@@ -198,10 +294,18 @@ signed main()
     }
 
     vector <production> plist;
-    plist.push_back(production(productions[1].lhs, productions[1].rhs, 4));
+    plist.push_back(production(productions[0].lhs, productions[0].rhs, 0));
     item_set is = item_set(plist);
     is.get_closure();
-    cout << is;
+    item_list.push_back(is);
+
+    generate(is);
+
+    for(item_set item : item_list)
+    {
+        cout << "I (" << item.id << ")" << endl;
+        cout << item << endl;
+    }
 
     return 0;
 }
